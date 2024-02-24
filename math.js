1716950d55fdd95b4d61a98b5d16538045727979
criruhe <158603770+criruhe@users.noreply.github.com>
2024-02-24 23:17:42 +0100

    Version 2 esPar

--- math.js
+++ math.js
@@ -1,8 +1,9 @@
 function esPar(numero) {
+  if (typeof numero !== 'number' || isNaN(numero)) {
+    throw new Error('El argumento debe ser un número válido.');
+  }
+  numero = Math.abs(numero);
   return numero % 2 === 0;
 }
 
-
-module.exports = {
-    esPar
-}
+module.exports = esPar;
