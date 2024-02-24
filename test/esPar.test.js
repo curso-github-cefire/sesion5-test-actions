1b876417fa75d103e0c223528039fdb4bc888f45
criruhe <158603770+criruhe@users.noreply.github.com>
2024-02-24 23:20:03 +0100

    Version 1 esPar.test.js

---
+++ test/esPar.test.js
@@ -0,0 +1,17 @@
+const esPar = require('./esPar');
+
+test('2 es par', () => {
+  expect(esPar(2)).toBe(true);
+});
+
+test('3 no es par', () => {
+  expect(esPar(3)).toBe(false);
+});
+
+test('Número negativo -4 es par', () => {
+  expect(esPar(-4)).toBe(true);
+});
+
+test('Texto no es par', () => {
+  expect(() => esPar('texto')).toThrow('El argumento debe ser un número válido.');
+});
